ca75c2b6ad601961fa79cd48e2579954428df9da
MamoruDS <mamoruds.io@gmail.com>
2026-03-25 22:47:17 +0900

    feat: add preview with `fsh` via `ZSH_HISTORY_FUZZY_SEARCH_FSH_PREVIEW`

--- zsh-history-fuzzy-search.zsh
+++ zsh-history-fuzzy-search.zsh
@@ -21,7 +21,12 @@ zsh-history-fuzzy-search() {
     local show_event_numbers=${ZSH_HISTORY_FUZZY_SEARCH_SHOW_EVENT_NUMBERS:-0}
 
     local preview_window_args=${ZSH_HISTORY_FUZZY_SEARCH_PREVIEW_WINDOW_ARGS:-"down:30%"}
-    local preview_pipe_cmd=${ZSH_HISTORY_FUZZY_SEARCH_PREVIEW_PIPE_CMD:-"cat"}
+    local preview_pipe_cmd
+    if [ "${ZSH_HISTORY_FUZZY_SEARCH_FSH_PREVIEW:-0}" -eq 1 ] && [ -d "$FAST_BASE_DIR" ]; then
+        preview_pipe_cmd="FAST_BASE_DIR='$FAST_BASE_DIR' FAST_WORK_DIR='$FAST_WORK_DIR' zsh '${0:h}/fsh-preview.zsh'"
+    else
+        preview_pipe_cmd=${ZSH_HISTORY_FUZZY_SEARCH_PREVIEW_PIPE_CMD:-"cat"}
+    fi
 
     command -v "$search_fuzzer" >/dev/null || return
 
